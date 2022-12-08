5e581cc40425f0c10835e581b4b76ea5d51dbb92
mohmyyy <iammohmy@gmail.com>
2022-12-08 17:22:52 +0530

    M4task9JS

--- Module 4/task9.js
+++ Module 4/task9.js
@@ -1,17 +1,61 @@
-// Function Currying in JS using bind
+1)	Write a function and use 'call' to call the function.
+let obj = {
+ nums : 10,
+ num1 : 25
+}
+let accessObj = function(x,y,z)
+{
+ return this.num1+this.nums+z+x+y;
+}
+console.log(accessObj.call(obj,10,15,20))
 
-// let func = function (x,y){
-// 	console.log((x*y));
-// }
-// let funcTwo = func.bind(this,10)
-// funcTwo(10)
+2) Write a program using apply.
+let obj = {
+ nums:22,
+ nums1:48
+}
+let objFunction = function(n,n1){
+ return this.nums + this.nums1 + n + n1;
+}
+let arr = [20,22]
+console.log(objFunction.apply(obj,arr))
 
-// function curring using function closure.
+3) Write a program using bind.
+let obj = {
+ num1 : 40,
+ num2 : 40
+}
+let objFunction = function (n1,n2){
+ return this.num1 + this.num2 + n1 + n2;
+}
+let bound = objFunction.bind(obj)
+console.log(bound(1,2))
+
+4) 
+let student = {
+ age : 20
+}
+let studentFunction = function() {
+ return this.age
+}
+let bound = studentFunction.bind(student);
+console.log(bound())
+
+
+Currying 
+// Function Currying using bind
+
+let func = function (x,y){
+  console.log((x*y));
+}
+let funcTwo = func.bind(this,10)
+funcTwo(10)
+// Function Curring using function closure.
 
 let multiple = function(x){
-	return function(y){
-		console.log(x*y);
-	}
+  return function(y){
+    console.log(x*y);
+  }
 }
 let multipleTwo = multiple(2)
 multipleTwo(4)
